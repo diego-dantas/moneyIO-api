961f922afd67501ee0497e01ce642d79a81ec661
Diego Dantas <Dantas@localhost.localdomain>
2018-01-08 21:49:03 -0200

    creation of repository

---
+++ src/main/java/in/blackscreen/moneyio/api/MoneyioApiApplication.java
@@ -0,0 +1,12 @@
+package in.blackscreen.moneyio.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyioApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyioApiApplication.class, args);
+	}
+}
