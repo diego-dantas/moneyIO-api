bd76abd7d79092c612147c88f57e3eafb0178222
Diego Dantas <Dantas@localhost.localdomain>
2018-01-08 22:56:10 -0200

    Integration with TravisCI

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
+[![Build Status](https://travis-ci.org/diego-dantas/moneyIO-api.svg?branch=master)](https://travis-ci.org/diego-dantas/moneyIO-api)
 # moneyIO-api
 moneyIO is an application for personal financial control 
--- src/main/java/in/blackscreen/moneyio/api/MoneyioApiApplication.java
+++ src/main/java/in/blackscreen/moneyio/api/MoneyioApiApplication.java
@@ -2,11 +2,22 @@ package in.blackscreen.moneyio.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class MoneyioApiApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(MoneyioApiApplication.class, args);
+
+	}
+
+	@GetMapping("/")
+	public String view(){
+		return "Hello MoneyIO";
 	}
 }
